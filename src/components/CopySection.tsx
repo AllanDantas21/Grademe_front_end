e445dfe0926df91ca0c15e4188339e3fa1788b48
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-25 21:40:17 -0300

    improve link section

--- src/components/CopySection.tsx
+++ src/components/CopySection.tsx
@@ -2,18 +2,18 @@ import { useCallback } from 'react'
 
 const CopySection: React.FC = () => {
     const handleCopy = useCallback(() => {
-        navigator.clipboard.writeText("Texto para copiar")
+        navigator.clipboard.writeText("source <(curl https://grademe.com.br/install.sh)")
     }, [])
 
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="text-black bg-gradient-to-r from-grey/50 to-grey/50 backdrop-blur rounded-xl shadow-xl flex items-center gap-2 w-96 h-16">
+            <div className="text-black bg-gradient-to-r from-grey/50 to-grey/50 backdrop-blur rounded-xl shadow-xl flex items-center gap-2 w-98 h-16 border border-gray-300 transition-all duration-500 transform hover:scale-105">
                 <div className="w-4/5 m-5">
-                    <a href="#" onClick={(e) => { e.preventDefault(); handleCopy() }}>Link para copiar</a>
+                    <a href="#" onClick={(e) => { e.preventDefault(); handleCopy() }}>source &lt;(curl https://grademe.com.br/install.sh)</a>
                 </div>
                 <button 
                     onClick={handleCopy} 
-                    className="h-full w-24 flex items-center rounded-r-xl justify-center bg-gradient-to-r from-blue-400 to-blue-200 via-blue-500 to-transparent bg-[length:200%_100%] bg-[position:100%_0] hover:bg-[position:0_0] transition-all duration-500"
+                    className="h-full w-24 flex items-center rounded-r-xl justify-center bg-gradient-to-r from-blue-400 to-blue-200 via-blue-500 to-transparent bg-[length:200%_100%] bg-[position:100%_0] hover:bg-[position:0_0] transition-all duration-500 transform hover:scale-105"
                 >
                     Copy
                 </button>
